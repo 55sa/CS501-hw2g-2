e97cc441fe0d32e9211ba1b004f4c3c80f78739f
hz_7279 <hzjh@bu.edu>
2024-09-12 12:39:50 -0400

    try to run #2

--- app/src/main/java/com/example/hw2g_2/MainActivity.kt
+++ app/src/main/java/com/example/hw2g_2/MainActivity.kt
@@ -18,17 +18,94 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Hw2g2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Calculator()
+        }
+    }
+}
+
+@Composable
+fun Calculator(){
+    Column{
+        val number1 = remember { mutableStateOf(0f) }
+        val number2 = remember { mutableStateOf(0f) }
+        val operation = remember { mutableStateOf("")}
+        val result = remember { mutableStateOf(0f) }
+
+        // val operation = remember { mutableStateOf("") }
+
+        TextField(
+            value = number1.value.toString(),
+            onValueChange = { number1.value = it.toFloat() },
+            label = { Text("Enter Number 1") }
+        )
+        TextField(
+            value = number2.value.toString(),
+            onValueChange = { number2.value = it.toFloat() },
+            label = { Text("Enter Number 2") }
+        )
+
+        TextField(
+            value = operation.value.toString(),
+            onValueChange = { operation.value = it },
+            label = { Text("Enter + - * / %") }
+        )
+
+        Text(text = "Operation: $operation")
+
+        Text(text = "Result: ${result.value}")
+
+        Row {
+            Button(onClick = {
+                // operation.value = "+"
+                if operation.value == "-" {
+                    result.value = Subtract(number1.value, number2.value)
                 }
+                else if (operation.value == "*") {
+                    result.value = Multiply(number1.value, number2.value)
+                }
+                else if (operation.value == "/") {
+                    if number2.value == 0{
+                        result.value = "Cannot divide by zero"
+                    }
+                    result.value = Deviation(number1.value, number2.value)
+                }
+                else if (operation.value == "%") {
+                    result.value = Modulo(number1.value, number2.value)
+                }
+                else if (operation.value == "+") {
+                    result.value = Add(number1.value, number2.value)
+                }
+                else {
+                    result.value = "Invalid Operation"
+                }
+                result.value = Add(number1.value, number2.value)
+            }) {
+                Text("Calculate")
             }
         }
     }
 }
+
+fun Multiply(x: Float, y: Float){
+    return (x * y).toFloat()
+}
+
+fun Add(x: Float, y: Float){
+    return (x + y).toFloat()
+}
+
+fun Subtract(x: Float, y: Float){
+    return (x - y).toFloat()
+}
+
+fun Modulo(x: Float, y: Float): Float{
+    return (x % y).toFloat()
+}
+
+fun Deviation(x: Float, y: Float): Float{
+    return (x / y).toFloat()
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
