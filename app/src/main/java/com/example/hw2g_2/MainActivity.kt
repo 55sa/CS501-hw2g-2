39c5fb94dc99171bfabc3f3b008e2967121a5720
charlesson7777 <123232898+charlesson7777@users.noreply.github.com>
2024-09-12 14:59:15 -0400

    built a box

--- app/src/main/java/com/example/hw2g_2/MainActivity.kt
+++ app/src/main/java/com/example/hw2g_2/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.hw2g_2
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hw2g_2.ui.theme.Hw2g2Theme
+import androidx.compose.foundation.background
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +30,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Hw2g2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+               MainScreen()
                 }
             }
+        }
+    }
+
+
+
+
+@Composable
+fun MainScreen(){
+    var boxVisible by remember {
+        mutableStateOf(true)
+    }
+
+    val onClick = {newState: Boolean ->
+        boxVisible = newState
+    }
+
+    Column(
+        Modifier.padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            CustomButton(text = "ShowBox", targetState = true, onClick = onClick)
+            CustomButton(text = "HideBox", targetState = false, onClick = onClick)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        if(boxVisible){
+            Box(modifier = Modifier.size(height = 400.dp, width = 400.dp).background(Color.Gray))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CustomButton(text: String, targetState: Boolean, onClick: (Boolean) -> Unit, bgColor: Color  = Color.Blue) {
+    Button(
+        onClick = { onClick(targetState) },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = bgColor,
+            contentColor = Color.White
+        )
+    ) {
+        Text(text)
+    }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+   @Composable
+   fun MainScreen(){
+
+   }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    Hw2g2Theme {
+        MainScreen()
+    }
+}
